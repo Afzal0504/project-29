ae3db5b1f2435b37aee88c468a4480e4a88d93e7
Afzal0504 <79651640+Afzal0504@users.noreply.github.com>
2021-08-12 20:54:02 +0530

    Add files via upload

---
+++ Box.js
@@ -0,0 +1,17 @@
+class Box extends BaseClass {
+  constructor(x, y, width, height){
+    super(x,y,width,height);
+    this.image = loadImage("sprites/wood1.png");
+    this.image1 = loadImage("sprites/polygon.png");
+  }
+
+  show(){
+    var angle = this.body.angle;
+    push();
+    translate(this.body.position.x, this.body.position.y);
+    rotate(angle);
+    imageMode(CENTER);
+    image(this.image1, 0, 0, this.width, this.height);
+    pop();
+  }
+};
